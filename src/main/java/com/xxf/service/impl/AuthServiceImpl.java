016ff218b59a3e516aa4c626bda73c59352af591
JerryX <xuefengxu333@foxmail.com>
2019-01-20 00:39:22 +0800

    模板消息bugfix2

--- src/main/java/com/xxf/service/impl/AuthServiceImpl.java
+++ src/main/java/com/xxf/service/impl/AuthServiceImpl.java
@@ -155,15 +155,19 @@ public class AuthServiceImpl implements AuthService {
         String time = df.format(new Date());
 
         Map<String, String> map = new LinkedHashMap<>();
-        map.put("value", brand + "拼单");
+        map.put("value", "CafeBabes: " + brand + "咖啡拼单");
         dataMap.put("keyword1", map);
-        map.put("value", brand + " " + size + "杯");
+        Map<String, String> map2 = new LinkedHashMap<>();
+        map2.put("value", brand + " " + size + "杯");
         dataMap.put("keyword2", map);
-        map.put("value", price + "元");
+        Map<String, String> map3 = new LinkedHashMap<>();
+        map3.put("value", price + "元");
         dataMap.put("keyword3", map);
-        map.put("value", nickname);
+        Map<String, String> map4 = new LinkedHashMap<>();
+        map4.put("value", nickname);
         dataMap.put("keyword4", map);
-        map.put("value", time);
+        Map<String, String> map5 = new LinkedHashMap<>();
+        map5.put("value", time);
         dataMap.put("keyword5", map);
         return dataMap;
     }
